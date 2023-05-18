0cb7c1d7231466a10b3d0651f683420dc28aa508
LaSauce <youssefyouss321@gmail.com>
2023-05-18 20:37:00 +0100

    deuxieme commit

--- src/main/java/com/ensakh/sitegestion/GestionApplication.java
+++ src/main/java/com/ensakh/sitegestion/GestionApplication.java
@@ -10,4 +10,7 @@ public class GestionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionApplication.class, args);
 	}
+	//des modifications ici
+	int i=0;
+	//fin.
 }
